00d67f9bfe06f82219e58dd0817467042e41c1b0
Nusha Developer <nuzhaimthikaf123@gmail.com>
2026-05-12 23:03:14 +0530

    fixed authentication error

--- backend/src/main/java/com/ecomica/backend/controller/AuthController.java
+++ backend/src/main/java/com/ecomica/backend/controller/AuthController.java
@@ -17,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.Instant;
 import java.util.Locale;
 
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
