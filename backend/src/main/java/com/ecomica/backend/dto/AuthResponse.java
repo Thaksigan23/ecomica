6e63b9d6a66e0fc044a2d4f7d0b946a50d684898
AtheeqCoder <atheeqnazeer99@gmail.com>
2026-05-13 11:12:00 +0530

    validation added

--- backend/src/main/java/com/ecomica/backend/dto/AuthResponse.java
+++ backend/src/main/java/com/ecomica/backend/dto/AuthResponse.java
@@ -1,13 +1,24 @@
 package com.ecomica.backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class AuthResponse {
+
+    @NotBlank(message = "Token cannot be empty")
     private String token;
+
+    @NotBlank(message = "Role is required")
     private String role;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
 }
